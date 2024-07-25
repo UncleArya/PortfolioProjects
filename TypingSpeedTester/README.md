4beb2a54b723108f09a88b37d1d8480b8f932ff6
UncleArya <100550741+UncleArya@users.noreply.github.com>
2024-07-25 15:48:13 -0700

    Project completion

--- TypingSpeedTester/README.md
+++ TypingSpeedTester/README.md
@@ -1,40 +1,28 @@
 # Typing Speed Tester
 
-## ~~Create Basic GUI~~
+## Skills Used:
 
--   ~~Window with boxes:~~
-    -   ~~Title~~
-    -   ~~Word prompt~~
-    -   ~~User entry~~
+-   Python
+    -   Tkinter
+    -   Custom class & functions
+    -   Loops
+    -   Complex calculations
+-   UI/UX Design
+-   Affinity Designer
 
-## ~~Program Functionality~~
+## Instructions:
 
--   ~~Register user input~~
--   ~~Display prompts~~
--   ~~Create word bank~~
--   ~~Detect word accuracy~~
+-   Place your cursor in the light grey box in the centre of the window
+-   Once you click on the _Start_ button, a 60 second timer will begin
+-   Hitting the _Space_ key submits your typed word and moves to the next word
+-   You can hit _Reset_ to stop a typing session in process, or to clear results after a finished session
 
-## Finalize GUI
+## Methodology:
 
--   ~~Display past and upcoming words~~
--   ~~Create a background image~~
--   ~~Advanced functions:~~
-    -   ~~Timer~~
-    -   ~~WPM~~
-    -   ~~Accuracy~~
+-   Words per Minute (WPM) is calculated by the most commonly accepted metric of:
+    -   Number of Words = (Characters Typed / 5)
+    -   WPM = (Number of Words / 60)
+-   A character includes punctuation
+-   [Further details on common WPM calculation](https://www.speedtypingonline.com/typing-equations)
 
-## Bug Fixes
-
--   ~~Put Entry Label in background, not in entry box~~
--   ~~Centre input in entry box~~
--   ~~Create end/reset functions~~
--   ~~Ensure start can only be hit if timer not active~~
--   ~~End screen when timer up~~
--   ~~Remove some punctuation (;")~~
--   ~~Make word bank bigger~~
--   ~~Calculate WPM on time update, not on space~~
--   Refactor into multiple files/classes:
-    -   GUI
-    -   Logic
--   Better names for functions
--   Docstrings
+**Credit:** Word prompts are taken from Chapter 1 of Alice's Adventures in Wonderland by Lewis Carroll. This work is in the public domain and made available by [Project Gutenberg](https://www.gutenberg.org/ebooks/11).
--- TypingSpeedTester/main.py
+++ TypingSpeedTester/main.py
@@ -4,14 +4,17 @@ import random
 from prompt_library import WORD_BANK
 
 
-class Word_Generator:
+# ---------- Program Execution ---------- #
+class Typing_Program:
     def __init__(self):
+        """Contains the functions required to execute the typing program."""
         self.word_bank = WORD_BANK
         self.starting_time = 60
         self.elapsed_time = 0
         self.timer_running = False
 
     def start_button(self):
+        """Button functionality to start the program. Randomly selects words from the work bank, sets counters to 0, and starts a timer."""
         self.timer_running = True
         if self.elapsed_time == 0:
             self.current_paragraph = random.randint(0, len(self.word_bank) - 1)
@@ -25,6 +28,7 @@ class Word_Generator:
             self.update_words()
 
     def reset_button(self):
+        """Button functionality to stop/reset the program. Stops and resets the timer, clears GUI elements."""
         self.timer_running = False
         self.starting_time = 60
         self.elapsed_time = 0
@@ -39,16 +43,19 @@ class Word_Generator:
         user_input_label.config(state="normal")
 
     def update_words(self):
+        """Moves the sentence forward by one word."""
         self.current_word_index += 1
         self.active_word()
         self.previous_word()
         self.next_word()
 
     def active_word(self):
+        """Applies style to active word typist must duplicate."""
         self.displayed_word = self.word_bank[self.current_paragraph].split(" ")[self.current_word_index]
         canvas.itemconfig(active_word_label, text=self.displayed_word)
 
     def previous_word(self):
+        """Applies style to most previous word typist attempted to duplicate."""
         if self.current_word_index < 1:
             self.previous_displayed_word = ""
         else:
@@ -58,13 +65,15 @@ class Word_Generator:
         canvas.itemconfig(previous_word_label, text=self.previous_displayed_word)
 
     def next_word(self):
+        """Applies style to next word the typist will be asked to duplicate."""
         current_word_length = len(self.displayed_word)
         new_x_location = ((current_word_length + 1) * 17) + 380
         self.next_displayed_word = self.word_bank[self.current_paragraph].split(" ")[self.current_word_index + 1]
-        canvas.coords(next_word_label, new_x_location, 200)
+        canvas.coords(next_word_label, new_x_location, 375)
         canvas.itemconfig(next_word_label, text=self.next_displayed_word)
 
     def on_space(self, event):
+        """When user hits space key, the user input is obtained, checked for accuracy, added to words per minute count, and requests next word."""
         if self.timer_running:
             self.user_input = user_input_var.get()
             self.user_input_no_spaces = user_input_var.get().strip()
@@ -74,15 +83,17 @@ class Word_Generator:
             self.update_words()
 
     def on_entry_click(self, event):
+        """Clears entry box when cursor is inserted."""
         if user_input_var.get() != "":
             user_input_label.delete(0, "end")
 
     def count_characters(self, word):
+        """Count number of characters in user inputted word."""
         word_characters = len(word)
         self.total_character_count += word_characters
-        # self.update_word_score()
 
     def check_accuracy(self, word):
+        """Check if user inputted word matches the requested word."""
         self.total_word_count += 1
         if self.displayed_word == word:
             self.correct_word_count += 1
@@ -92,15 +103,18 @@ class Word_Generator:
         self.update_accuracy()
 
     def update_word_score(self):
+        """Update UI element for words per minute calculation."""
         if self.total_word_count > 0:
             words_per_minute = int(round((self.total_character_count / 5) / (self.elapsed_time / 60), 0))
             canvas.itemconfig(word_score, text=words_per_minute)
 
     def update_accuracy(self):
+        """Update UI element for user word accuracy"""
         word_accuracy = int(round((self.correct_word_count / self.total_word_count) * 100, 0))
         canvas.itemconfig(accuracy, text=word_accuracy)
 
     def timer(self, time, time_elapsed):
+        """Timer mechanism for typing program"""
         if self.timer_running:
             self.elapsed_time = time_elapsed
             canvas.itemconfig(timer, text=time)
@@ -116,8 +130,7 @@ class Word_Generator:
                 canvas.itemconfig(starting_word_label, text="Done! Hit 'Reset' To Go Again!")
 
 
-word = Word_Generator()
-
+run = Typing_Program()
 
 # ---------- GUI ---------- #
 # Constants
@@ -149,69 +162,69 @@ canvas.config(highlightthickness=0, borderwidth=0)
 canvas.place(x=0, y=0)
 
 # Title
-title_text = canvas.create_text(400, 75, anchor=CENTER, text="Arya's Typing Tester")
+title_text = canvas.create_text(400, 100, anchor=CENTER, text="Arya's Typing Tester")
 canvas.itemconfig(title_text, font=H1_FONT, fill=TEXT_COLOUR)
 
 # Word Prompt
-active_word_label = canvas.create_text(380, 200, anchor=W, text="")
+active_word_label = canvas.create_text(380, 375, anchor=W, text="")
 canvas.itemconfig(active_word_label, font=H2_FONT, fill=ACTIVE_TEXT_COLOUR)
-previous_word_label = canvas.create_text(360, 200, anchor=E, text="")
+previous_word_label = canvas.create_text(360, 375, anchor=E, text="")
 canvas.itemconfig(previous_word_label, font=H2_FONT, fill=TEXT_COLOUR)
-next_word_label = canvas.create_text(380, 200, anchor=W, text="")
+next_word_label = canvas.create_text(380, 375, anchor=W, text="")
 canvas.itemconfig(next_word_label, font=H2_FONT, fill=TEXT_ACCENT_COLOUR)
-starting_word_label = canvas.create_text(400, 200, anchor=CENTER, text="Ready? Hit 'Start' To Begin!")
+starting_word_label = canvas.create_text(400, 375, anchor=CENTER, text="Ready? Hit 'Start' To Begin!")
 canvas.itemconfig(starting_word_label, font=H2_FONT, fill=TEXT_COLOUR)
 
 # User Input
 user_input_var = StringVar()
 user_input_var.set("Place Cursor Here")
 user_input_label = Entry(window, textvariable=user_input_var)
-user_input_label.bind("<space>", word.on_space)
-user_input_label.bind("<FocusIn>", word.on_entry_click)
+user_input_label.bind("<space>", run.on_space)
+user_input_label.bind("<FocusIn>", run.on_entry_click)
 user_input_label.config(bg=BG_LIGHT, width=40, highlightthickness=0, border=0)
 user_input_label.config(justify=CENTER, font=BODY_FONT, fg=TEXT_COLOUR, insertbackground=TEXT_COLOUR)
-canvas.create_window(400, 350, window=user_input_label)
+canvas.create_window(400, 500, window=user_input_label)
 
 # Timer
-timer_label = canvas.create_text(150, 477, anchor=CENTER, text="Time Remaining")
+timer_label = canvas.create_text(150, 602, anchor=CENTER, text="Time Remaining")
 canvas.itemconfig(timer_label, font=H3_FONT, fill=TEXT_COLOUR)
-timer = canvas.create_text(150, 580, anchor=CENTER, text="--")
+timer = canvas.create_text(150, 705, anchor=CENTER, text="--")
 canvas.itemconfig(timer, font=H1_FONT, fill=TEXT_COLOUR)
 
 # Word Score
-word_score_label = canvas.create_text(400, 477, anchor=CENTER, text="Words Per Minute")
+word_score_label = canvas.create_text(400, 602, anchor=CENTER, text="Words Per Minute")
 canvas.itemconfig(word_score_label, font=H3_FONT, fill=TEXT_COLOUR)
-word_score = canvas.create_text(400, 580, anchor=CENTER, text="--")
+word_score = canvas.create_text(400, 705, anchor=CENTER, text="--")
 canvas.itemconfig(word_score, font=H1_FONT, fill=TEXT_COLOUR)
 
 # Accuracy
-accuracy_label = canvas.create_text(650, 477, anchor=CENTER, text="Accuracy (%)")
+accuracy_label = canvas.create_text(650, 602, anchor=CENTER, text="Accuracy (%)")
 canvas.itemconfig(accuracy_label, font=H3_FONT, fill=TEXT_COLOUR)
-accuracy = canvas.create_text(650, 580, anchor=CENTER, text="--")
+accuracy = canvas.create_text(650, 705, anchor=CENTER, text="--")
 canvas.itemconfig(accuracy, font=H1_FONT, fill=TEXT_COLOUR)
 
 # Start Button
-start_button = Button(text="START", command=word.start_button)
+start_button = Button(text="START", command=run.start_button)
 start_button.config(bg=BG_LIGHT, padx=19, pady=9, highlightthickness=0, border=0, activebackground=BG_LIGHT)
 start_button.config(fg=ACTIVE_TEXT_COLOUR, activeforeground=TEXT_ACCENT_COLOUR, font=H3_FONT)
-canvas.create_window(100, 679, window=start_button)
+canvas.create_window(100, 204, window=start_button)
 
 # Reset Button
-reset_button = Button(text="RESET", command=word.reset_button)
+reset_button = Button(text="RESET", command=run.reset_button)
 reset_button.config(bg=BG_LIGHT, padx=19, pady=9, highlightthickness=0, border=0, activebackground=BG_LIGHT)
 reset_button.config(fg=ACTIVE_TEXT_COLOUR, activeforeground=TEXT_ACCENT_COLOUR, font=H3_FONT)
-canvas.create_window(100, 754, window=reset_button)
+canvas.create_window(100, 279, window=reset_button)
 
 # Instructions
-instructions_label = canvas.create_text(475, 670, anchor=CENTER, text="~~ Instructions ~~")
+instructions_label = canvas.create_text(475, 195, anchor=CENTER, text="~~~ Instructions ~~~")
 canvas.itemconfig(instructions_label, font=H3_FONT, fill=TEXT_ACCENT_COLOUR)
-instructions_line_1 = canvas.create_text(210, 695, anchor=W, text="~ Place cursor in centre box and hit 'Start' button")
+instructions_line_1 = canvas.create_text(210, 220, anchor=W, text="~ Place cursor in centre box and hit 'Start' button")
 canvas.itemconfig(instructions_line_1, font=H4_FONT, fill=TEXT_ACCENT_COLOUR)
-instructions_line_2 = canvas.create_text(210, 715, anchor=W, text="~ Timer will run for 60 seconds")
+instructions_line_2 = canvas.create_text(210, 240, anchor=W, text="~ Timer will run for 60 seconds")
 canvas.itemconfig(instructions_line_2, font=H4_FONT, fill=TEXT_ACCENT_COLOUR)
-instructions_line_3 = canvas.create_text(210, 735, anchor=W, text="~ Hitting 'Space' moves to next word")
+instructions_line_3 = canvas.create_text(210, 260, anchor=W, text="~ Hitting 'Space' moves to next word")
 canvas.itemconfig(instructions_line_3, font=H4_FONT, fill=TEXT_ACCENT_COLOUR)
-instructions_line_4 = canvas.create_text(210, 755, anchor=W, text="~ Hit 'Reset' button to stop and go again")
+instructions_line_4 = canvas.create_text(210, 280, anchor=W, text="~ Hit 'Reset' button to stop and go again")
 canvas.itemconfig(instructions_line_4, font=H4_FONT, fill=TEXT_ACCENT_COLOUR)
 
 window.mainloop()
