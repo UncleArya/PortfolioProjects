56eeda8191d21d7b01b84f3a39b2ca63ab51361e
UncleArya <100550741+UncleArya@users.noreply.github.com>
2024-08-05 16:05:46 -0700

    Created a web GUI to display info with Flask

--- CostOfLivingAnalysis/README.md
+++ CostOfLivingAnalysis/README.md
@@ -1,1 +1,31 @@
 # Cost of Living Analysis
+
+A financial analysis of the cost of living in Victoria, BC is done using web scraping and displayed using a basic web GUI. Using the newly obtained data, financial information is displayed about real estate prices, average wages, and estimated expenses involved in the costs of living. Dynamic and real-time calculations are then made analyzing scenarios such as: how long it would take to save for a condo down payment or pay off a mortgage at an average wage, and how long to save for a down payment when working a minimum wage job.
+
+## Skills Used:
+
+-   Python
+    -   Requests
+    -   CSV
+    -   Custom Classes and Functions
+    -   Flask Bootstrap
+-   Data Science
+    -   Statistics
+-   Web Scraping
+    -   BeautifulSoup
+-   HTML
+-   CSS
+    -   Bootstrap
+-   UI/UX Design
+
+## Methodology:
+
+-   Each time the site is initialized, the following data is obtained:
+    -   Current listed prices for 1 bedroom condos in Victoria, BC
+    -   Current average hourly wages from 2 sources posting jobs in Victoria, BC and create an average wage from those sources
+    -   Current estimated monthly costs for a single person living in Victoria, BC
+-   Time to pay off a mortgage is based off the freshly obtained data and calculated using a 5% interest rate and 10% down payment
+-   Minimum wage used in calculations is: `$17.40`
+-   **_Note:_** all calculations are estimates and only meant for demonstration purposes
+
+## Screenshots
--- CostOfLivingAnalysis/main.py
+++ CostOfLivingAnalysis/main.py
@@ -2,6 +2,8 @@ from src.real_estate import Real_Estate
 from src.wages import Wages
 from src.expenses import Expenses
 from src.calculations import Calculations
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 # Constants
 REAL_ESTATE_CSV = "./data/home_prices.csv"
@@ -12,7 +14,11 @@ real_estate = Real_Estate()
 wages = Wages()
 expenses = Expenses()
 
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
+
+# Fetch an update to all data sets
 def update_data_sets():
     """Updates all 3 data sets and writes current data to respective CSV files."""
     real_estate.update_real_estate_data(file=REAL_ESTATE_CSV)
@@ -28,16 +34,25 @@ expenses_data = expenses.obtain_expense_data(file=EXPENSES_CSV)
 # Fetch Calculations
 calculations = Calculations(real_estate_data=real_estate_data, wage_data=wage_data, expenses_data=expenses_data)
 months_to_save_for_down_payment = calculations.average_wage_down_payment()
-years_needed_to_pay_off_mortage = calculations.years_to_pay_off_mortgage()
+years_needed_to_pay_off_mortgage = calculations.years_to_pay_off_mortgage()
 months_for_two_adults_down_payment = calculations.two_adults_down_payment()
 
-print(f"Current price of a condo: ${real_estate_data:,}")
-print(f"Current average wage in Victoria: ${wage_data}/hour")
-print(f"Current cost of living in Victoria: ${expenses_data:,}")
-print(
-    f"Time needed to save for a 10% down payment on a condo at average wage: {months_to_save_for_down_payment} months"
-)
-print(f"Years needed to pay off a mortgage for a condo at average wage: {years_needed_to_pay_off_mortage} years")
-print(
-    f"Months needed for two adults earning minimum wage to save for a 10% down payment on a condo: {months_for_two_adults_down_payment} months"
-)
+
+# Flask App
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        real_estate_data=real_estate_data,
+        wage_data=wage_data,
+        expenses_data=expenses_data,
+        months_to_save_for_down_payment=months_to_save_for_down_payment,
+        years_needed_to_pay_off_mortgage=years_needed_to_pay_off_mortgage,
+        months_for_two_adults_down_payment=months_for_two_adults_down_payment,
+    )
+
+
+update_data_sets()
+
+if __name__ == "__main__":
+    app.run(debug=True)
